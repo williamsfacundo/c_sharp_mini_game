d2262ba9bb1efdd06aee0ee60378dc89a347cb9a
facundo <facuwilli@gmail.com>
2022-05-06 11:35:02 -0300

    Added lives in player class.

--- Juego en CSharp/Juego/Player.cs
+++ Juego en CSharp/Juego/Player.cs
@@ -5,7 +5,10 @@ namespace Juego
     class Player : Character
     {    
         private short points;
+        private short lives;
+
         private const short pointsGained = 5;
+        private const short initialLives = 5;
 
         public short Points 
         {
@@ -19,9 +22,22 @@ namespace Juego
             }
         }
 
+        public short Lives
+        {
+            set
+            {
+                lives = value;
+            }
+            get
+            {
+                return lives;
+            }
+        }
+
         public Player(short x, short y) : base(x, y) 
         {
             Points = 0;
+            Lives = initialLives;
         }
 
         public void Input(short worldMinX, short worldMaxX, short worldMinY, short worldMaxY, ref ConsoleKeyInfo cki) 
